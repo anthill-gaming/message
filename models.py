f5cef0f8cc1662cbc5611a084b40c94db4c5f94f
Vladimir <x55aah@gmail.com>
2019-05-23 19:43:21 +0300

    updates

--- models.py
+++ models.py
@@ -3,11 +3,11 @@
 from anthill.framework.db import db
 from anthill.framework.utils import timezone
 from anthill.platform.api.internal import InternalAPIMixin
+from anthill.platform.auth import RemoteUser
 from anthill.framework.utils.translation import translate_lazy as _
 from anthill.framework.utils.asynchronous import as_future
 from anthill.framework.utils.functional import SimpleLazyObject
 from sqlalchemy_utils.types import ChoiceType, URLType
-from anthill.platform.auth import RemoteUser
 from functools import partial
 import re
 import six
@@ -66,12 +66,6 @@ MESSAGE_STATUSES = (
     ('read', _('Read')),
 )
 
-GROUP_TYPES = (
-    ('p', _('Personal')),
-    ('m', _('Multiple')),
-    ('c', _('Channel')),
-)
-
 
 class MessageStatus(InternalAPIMixin, db.Model):
     __tablename__ = 'message_statuses'
@@ -116,12 +110,12 @@ class Message(InternalAPIMixin, db.Model):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    sender_id = db.Column(db.Integer)
+    sender_id = db.Column(db.Integer, nullable=False)
+    group_id = db.Column(db.Integer, nullable=False)
     created = db.Column(db.DateTime, default=timezone.now)
     updated = db.Column(db.DateTime, onupdate=timezone.now)
     active = db.Column(db.Boolean, nullable=False, default=True)
     draft = db.Column(db.Boolean, nullable=False, default=False)
-    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
     statuses = db.relationship('MessageStatus', backref='message', lazy='dynamic')
     reactions = db.relationship('MessageReaction', backref='message', lazy='dynamic')
     discriminator = db.Column(db.String)
@@ -200,74 +194,3 @@ class URLMessage(Message):
     __mapper_args__ = {
         'polymorphic_identity': 'url_message',
     }
-
-
-class Group(db.Model):
-    __tablename__ = 'groups'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(128), nullable=False, unique=True)
-    type = db.Column(ChoiceType(GROUP_TYPES))
-    created = db.Column(db.DateTime, default=timezone.now)
-    updated = db.Column(db.DateTime, onupdate=timezone.now)
-    active = db.Column(db.Boolean, nullable=False, default=True)
-    messages = db.relationship('Message', backref='group', lazy='dynamic')
-
-    @as_future
-    def get_messages(self, user_id=None, **kwargs):
-        default_kwargs = dict(active=True)
-        if user_id is not None:
-            default_kwargs.update(sender_id=user_id)
-        default_kwargs.update(kwargs)
-        return self.messages.filter_by(**default_kwargs)
-
-    @as_future
-    def get_memberships(self, user_id=None, **kwargs):
-        default_kwargs = dict(active=True)
-        if user_id is not None:
-            default_kwargs.update(user_id=user_id)
-        default_kwargs.update(kwargs)
-        return self.memberships.filter_by(**default_kwargs)
-
-
-class GroupMembership(InternalAPIMixin, db.Model):
-    __tablename__ = 'groups_memberships'
-
-    group_id = db.Column(db.Integer, db.ForeignKey('groups.id', ondelete='CASCADE'), primary_key=True)
-    user_id = db.Column(db.Integer, primary_key=True)
-    created = db.Column(db.DateTime, default=timezone.now)
-    active = db.Column(db.Boolean, nullable=False, default=True)
-    group = db.relationship('Group', backref=db.backref('memberships', lazy='dynamic'))
-    notify_by_message = db.Column(db.Boolean, nullable=False, default=True)
-    notify_by_email = db.Column(db.Boolean, nullable=False, default=False)
-    # TODO: permissions
-
-    @property
-    def request_user(self):
-        return partial(self.internal_request, 'login', 'get_user')
-
-    async def get_receiver(self) -> RemoteUser:
-        data = await self.request_user(user_id=self.user_id)
-        return RemoteUser(**data)
-
-
-@as_future
-def get_friends(user_id):
-    membership_query = GroupMembership.query.join(Group)
-    friendships1 = membership_query.filter(Group.type == 'p').filter_by(active=True, user_id=user_id)
-    friendships2 = membership_query.filter(
-        GroupMembership.user_id != user_id, Group.id.in_(f.group_id for f in friendships1))
-    return (f.user_id for f in friendships2)
-
-
-@as_future
-def make_friends(user_id1, user_id2):
-    # TODO: check if already friends
-    group = Group.create(type='p', name=None)  # TODO: name
-    GroupMembership.create(user_id=user_id1, group_id=group.id)
-    GroupMembership.create(user_id=user_id2, group_id=group.id)
-
-
-@as_future
-def remove_friends(user_id1, user_id2):
-    Group.query.filter_by(type='p', name=None).first().delete()  # TODO: name
