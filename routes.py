6c4f3a5988610dda38a281e949245b93b67b8bb0
Vladimir <x55aah@gmail.com>
2019-07-04 19:03:25 +0300

    add rest api url

--- routes.py
+++ routes.py
@@ -1,14 +1,17 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from message import handlers
+from .api.compat.rest import routes as rest_routes
+from anthill.framework.utils.urls import include
 from anthill.framework.handlers.socketio import socketio_server
 from anthill.platform.core.messenger.handlers.transports import socketio
+from . import handlers
 
 MESSENGER_NAMESPACE = '/messenger'
 
 socketio_server.register_namespace(handlers.MessengerNamespace(MESSENGER_NAMESPACE))
 
 route_patterns = [
+    url(r'^/', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
     url(r'^/socket.io/$', socketio.MessengerHandler),
 ]
