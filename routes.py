ed2e93bfebc27fd85d6551d3378344900046e87c
Vladimir <x55aah@gmail.com>
2019-07-17 17:57:55 +0300

    updates

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from .api.compat.rest import routes as rest_routes
+from .api.v1.rest import routes as rest_routes
 from anthill.framework.utils.urls import include
 from anthill.framework.handlers.socketio import socketio_server
 from anthill.platform.core.messenger.handlers.transports import socketio
@@ -12,6 +12,6 @@ MESSENGER_NAMESPACE = '/messenger'
 socketio_server.register_namespace(handlers.MessengerNamespace(MESSENGER_NAMESPACE))
 
 route_patterns = [
-    url(r'^/', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
+    url(r'^/api/v1', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
     url(r'^/socket.io/$', socketio.MessengerHandler),
 ]
